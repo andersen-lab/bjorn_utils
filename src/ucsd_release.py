322b56ebe7feaa2023ff8c018f1fd5d7cecb9626
kramesh1 <43083703+kramesh1@users.noreply.github.com>
2021-12-28 08:15:08 -0800

    adding file changes for troubleshooting

--- src/ucsd_release.py
+++ src/ucsd_release.py
@@ -26,43 +26,43 @@ if __name__=="__main__":
     variants_path = "variants/illumina/"
     report_path = "trimmed_bams/illumina/reports/"
 
-    for path in [cons_path, bams_path, variants_path, report_path]:
-        os.makedirs(os.path.join(dir_name, path))
+    # for path in [cons_path, bams_path, variants_path, report_path]:
+    #     os.makedirs(os.path.join(dir_name, path))
     
     #define file endings
     variant_file_ending = "_L001_L002.tsv"
     consensus_file_ending = "_L001_L002.fa"
     bam_file_ending = "_L001_L002.sorted.bam"
 
-    for row in range(len(data.index)):
-        # get out the relevant info so we can use it on subprocess
-        search_id = data.iloc[row, 0]
-        variant_file_url = data.iloc[row, 1]
-        consensus_file_url = data.iloc[row, 2]
-        bam_file_url = data.iloc[row, 3]
+    # for row in range(len(data.index)):
+    #     # get out the relevant info so we can use it on subprocess
+    #     search_id = data.iloc[row, 0]
+    #     variant_file_url = data.iloc[row, 1]
+    #     consensus_file_url = data.iloc[row, 2]
+    #     bam_file_url = data.iloc[row, 3]
 
-        #TODO: Needs logging + multi-process
+    #     #TODO: Needs logging + multi-process
 
-        subprocess.run(
-            ["aws", "s3", "cp", variant_file_url, os.path.join(dir_name, variants_path, "".join([search_id, variant_file_ending]))]
-        )
+    #     subprocess.run(
+    #         ["aws", "s3", "cp", variant_file_url, os.path.join(dir_name, variants_path, "".join([search_id, variant_file_ending]))]
+    #     )
 
-        subprocess.run(
-            ["aws", "s3", "cp", consensus_file_url, os.path.join(dir_name, cons_path, "".join([search_id, consensus_file_ending]))]
-        )
+    #     subprocess.run(
+    #         ["aws", "s3", "cp", consensus_file_url, os.path.join(dir_name, cons_path, "".join([search_id, consensus_file_ending]))]
+    #     )
 
-        subprocess.run(
-            ["aws", "s3", "cp", bam_file_url, os.path.join(dir_name, bams_path, "".join([search_id, bam_file_ending]))]
-        )
+    #     subprocess.run(
+    #         ["aws", "s3", "cp", bam_file_url, os.path.join(dir_name, bams_path, "".join([search_id, bam_file_ending]))]
+    #     )
     
-    #TODO: Test if this works and then replace with a pythonic solution
-    os.chdir(os.path.join(dir_name, "consensus_sequences", "illumina"))
-    subprocess.run(
-        ["gawk", "-i", "inplace", "'/^>/{print'", '">Consensus_"', 'substr(FILENAME,1,length(FILENAME)-3)"_threshold_0.5_quality_20";', 'next}', "1'" , "*.fa"]
-    )
+    # #TODO: Test if this works and then replace with a pythonic solution
+    # os.chdir(os.path.join(dir_name, "consensus_sequences", "illumina"))
+    # subprocess.run(
+    #     ["gawk", "-i", "inplace", "'/^>/{print'", '">Consensus_"', 'substr(FILENAME,1,length(FILENAME)-3)"_threshold_0.5_quality_20";', 'next}', "1'" , "*.fa"]
+    # )
 
     #TODO: Test if this works
-    os.chdir(dir_name, report_path)
+    os.chdir(os.path.join(dir_name, report_path))
     report_file_url = "s3://ucsd-all/phylogeny/" + sys.argv[1].split(".")[0] + "/" + sys.argv[1].split(".")[0] + ".full_summary.csv"
     subprocess.run(
         ["aws", "s3", "cp", report_file_url, os.path.join(dir_name, report_path, "full_summary.csv")]
