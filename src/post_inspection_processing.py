e28614b10bc4cd47d21e4030f32903ef948761d3
kramesh1 <43083703+kramesh1@users.noreply.github.com>
2021-12-28 09:00:25 -0800

    small todo change

--- src/post_inspection_processing.py
+++ src/post_inspection_processing.py
@@ -94,7 +94,7 @@ def unalign_fasta(combined_aligned_fasta: str, combined_unaligned_fasta: str) ->
                     outfile.write(line.replace("-", ""))
     return
 
-#TODO: Some bug where we don't get the full number of sequences
+#TODO: Test if this works correctly with new Bio usage
 def multifasta_to_fasta(combined_unaligned_fasta: str) -> None:
     """
     Takes a combined fasta and splits it into separate files in a "consensus
