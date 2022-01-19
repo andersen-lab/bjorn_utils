ec1232429ae8ac478481f51fecb0e81eca331fc7
kramesh1 <43083703+kramesh1@users.noreply.github.com>
2022-01-19 07:47:15 -0800

    making ucsd release pythonic for s3 downloads

--- src/ucsd_release.py
+++ src/ucsd_release.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import sys
 
+import boto3
+
 from pandas.core.reshape.merge import merge_ordered
 
 if __name__=="__main__":
@@ -29,45 +31,45 @@ if __name__=="__main__":
     for path in [cons_path, bams_path, variants_path, report_path]:
         os.makedirs(os.path.join(dir_name, path))
     
-    #define file endings
+    # define file endings
     variant_file_ending = "_L001_L002.tsv"
     consensus_file_ending = "_L001_L002.fa"
     bam_file_ending = "_L001_L002.sorted.bam"
 
-    for row in range(len(data.index)):
-        #TODO: Precalculate all the commands, add them to a list, and then have 10 processes work on them concurrently or something
-        # get out the relevant info so we can use it on subprocess
-        search_id = data.iloc[row, 0]
-        variant_file_url = data.iloc[row, 1]
-        consensus_file_url = data.iloc[row, 2]
-        bam_file_url = data.iloc[row, 3]
+    # initiate boto3
+    s3 = boto3.client('s3')
+    bucket_name = "ucsd-all"
+    print("S3 connection complete")
 
-        #TODO: Needs logging + multi-process, or to be called via boto3
+    # iterate through downloads for each command type
 
-        subprocess.run(
-            ["aws", "s3", "cp", variant_file_url, os.path.join(dir_name, variants_path, "".join([search_id, variant_file_ending]))]
-        )
+    # variant commands
+    for row in zip(data["SEARCH SampleID"].to_list(), data["Variant File S3 URL"].to_list()):
+        s3.download_file(bucket_name, row[1].split("ucsd-all/")[1], os.path.join(dir_name, variants_path, "".join([row[0], variant_file_ending])))
+    print("Variant file transfer complete")
 
-        subprocess.run(
-            ["aws", "s3", "cp", consensus_file_url, os.path.join(dir_name, cons_path, "".join([search_id, consensus_file_ending]))]
-        )
+    # consensus commands
+    for row in zip(data["SEARCH SampleID"].to_list(), data["Consensus File S3 URL"].to_list()):
+        s3.download_file(bucket_name, row[1].split("ucsd-all/")[1], os.path.join(dir_name, cons_path, "".join([row[0], consensus_file_ending])))
+    print("Consensus file transfer complete")
 
-        subprocess.run(
-            ["aws", "s3", "cp", bam_file_url, os.path.join(dir_name, bams_path, "".join([search_id, bam_file_ending]))]
-        )
-    
+    # bam commands
+    for row in zip(data["SEARCH SampleID"].to_list(), data["BAM File S3 URL"].to_list()):
+        s3.download_file(bucket_name, row[1].split("ucsd-all/")[1], os.path.join(dir_name, bams_path, "".join([row[0], bam_file_ending])))
+    print("BAM file transfer complete")
+
+    # Rename FASTA headers for clarity    
     #TODO: Replace this with a pythonic equivalent
     os.chdir(os.path.join(dir_name, "consensus_sequences", "illumina"))
     replace_command = '''gawk -i inplace '/^>/{print ">Consensus_" substr(FILENAME,1,length(FILENAME)-3)"_threshold_0.5_quality_20"; next} 1' *.fa'''
     subprocess.call(replace_command, shell=True)
 
+    # Get the coverage report and refactor it
     os.chdir(os.path.join(dir_name, report_path))
     # get the files name without folder
     base_name = os.path.basename(sys.argv[1])
-    report_file_url = os.path.join("s3://ucsd-all/phylogeny/", base_name.split(".")[0], base_name.split(".")[0] + ".full_summary.csv")
-    subprocess.run(
-        ["aws", "s3", "cp", report_file_url, os.path.join(dir_name, report_path, "full_summary.csv")]
-        )
+    report_file_url = os.path.join("phylogeny/", base_name.split(".")[0], base_name.split(".")[0] + ".full_summary.csv")
+    s3.download_file(bucket_name, report_file_url, os.path.join(dir_name, report_path, "full_summary.csv"))
     summary = pd.read_csv("full_summary.csv")[["bam_name", "bjorn_coverage", "bjorn_avg_depth", "bjorn_min_coverage", "bjorn_max_coverage", "bjorn_num_w_zero_coverage"]]
     summary.rename(
         columns={
