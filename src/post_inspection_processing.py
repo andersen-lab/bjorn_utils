4dadc721c3a64461744a3a84ce175316bd303928
kramesh1 <43083703+kramesh1@users.noreply.github.com>
2022-01-20 16:16:28 -0800

    adjusting column import

--- src/post_inspection_processing.py
+++ src/post_inspection_processing.py
@@ -177,11 +177,11 @@ if __name__ == "__main__":
 
     # check Pangolin lineage timings for the current set of sequences
     subprocess.run(["./update_pangolineages_subset.sh", os.path.join(sys.argv[1], "msa")])
-    lineage_report = pd.read_csv(os.path.join(sys.argv[1], "msa", "lineage_report.csv"))
+    lineage_report = pd.read_csv(os.path.join(sys.argv[1], "msa", "lineage_report.csv"))[['taxon', "scorpio_call"]]
     metadata_reduced = data[["covv_virus_name", "covv_collection_date"]] 
     metadata_reduced["taxon"] = [item.split("/")[2] for item in metadata_reduced["covv_virus_name"]]
     meta_lineage_merge = metadata_reduced.merge(lineage_report, how="left", on="taxon")
-    pango_df = pd.read_csv("../lineage_reference.csv")[['taxon', "scorpio_call"]]
+    pango_df = pd.read_csv("../lineage_reference.csv")
     meta_lineage_reference_merge = meta_lineage_merge.merge(pango_df, how="left", on="scorpio_call")
     test_frame = meta_lineage_reference_merge[meta_lineage_reference_merge["covv_collection_date"] < meta_lineage_reference_merge["reference_date"]]
     if len(test_frame) > 0:
